3a0fbde4d037174fecfc4131d63fb6328f686a95
Johannes Heinz <johannes.heinz@diva-e.com>
2022-04-06 14:46:30 +0200

    Setup initial scaffold using Bulma

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { GraphQLModule } from './graphql.module';
+import { HttpClientModule } from '@angular/common/http';
+import { TableComponent } from './table/table.component';
+import { AboutComponent } from './about/about.component';
+import { InputComponent } from './input/input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableComponent,
+    AboutComponent,
+    InputComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    GraphQLModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
